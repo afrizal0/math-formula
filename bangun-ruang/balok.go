18baabe14fe0950e7a4bacdf9a37a920181dbdeb
MUH. AFRIZAL <87596110+afrizal0@users.noreply.github.com>
2022-03-14 01:39:09 +0800

    Update balok.go

--- bangun-ruang/balok.go
+++ bangun-ruang/balok.go
@@ -7,5 +7,5 @@ func Luas_Permukaan_Balok(p, l, t int) int {
 	return 2 * (p*l + p*t + l*t)
 }
 func Panjang_Balok(p, l, t int) int {
-	return (p * l * t) / l / t
+	return (Volume_Balok(p, l, t)) / l / t
 }
